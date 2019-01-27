a6ccff2d2eac3be9be491ecc6264a1ba15c02e13
kekonen <daniil.naumetc@gmail.com>
2019-01-27 16:40:25 +0100

    server to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::net::UdpSocket;
+use std::str;
+use std::net::{ToSocketAddrs, SocketAddr};
 
 pub fn serve() -> std::io::Result<()> {
     {
@@ -16,4 +18,178 @@ pub fn serve() -> std::io::Result<()> {
         socket.send_to(buf, &src)?;
     } // the socket is closed here
     Ok(())
+}
+
+#[derive(Debug)]
+pub struct Server{
+    address: SocketAddr,
+    socket: UdpSocket,
+}
+
+
+impl Server{
+    pub fn init(address: SocketAddr) -> Server {
+        let mut socket = UdpSocket::bind(address).unwrap();
+
+        Server{address: address, socket: socket}
+    }
+
+    pub fn receive(&self) -> std::io::Result<String> {
+        let mut buf = [0; 10];
+        let (amt, src) = self.socket.recv_from(&mut buf)?;
+        // println!("add: {:?}", src);
+
+        let buf = &mut buf[..amt];
+        let buf_str = str::from_utf8(&buf).unwrap();
+        Ok(buf_str.to_string())
+    }
+
+    pub fn send(&self, to_send: &str, address: SocketAddr) -> std::io::Result<()> {
+        {
+            // let buf = &mut to_send[..amt];
+            // buf.reverse();
+            // let addr = SocketAddr::from(([127, 0, 0, 1], *port));
+            // println!("addr1: {:?}", addr);
+
+            self.socket.send_to(to_send.as_bytes(), address)?;
+            // println!("r1: {:?}---{:?}", result, to_send.as_bytes());
+        }
+        Ok(())
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct A{
+	x: f32,
+	y: f32,
+	z: f32,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct V{
+	x: f32,
+	y: f32,
+	z: f32,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum EnvResult<T>{
+	Done,
+	Some(T)
+}
+
+
+#[derive(Debug, Copy, Clone)]
+pub struct State { // may be Radians for pitch/roll/heading? but for now f32
+	pitch:     f32,
+	roll:      f32,
+	heading:   f32, 
+	altitude:  f32,
+	latitude:  f32,
+	longitude: f32,
+	a:           A,
+	v:           V, 
+}
+
+// impl<T> Clone for State<T> {
+//     fn clone(&self) -> State<T> { *self }
+// }
+
+#[derive(Debug, Copy, Clone)]
+pub struct Action{
+	throttle:      f32,
+	aileron:       f32,
+	elevator:      f32,
+	rudder:        f32,
+	aileron_trim:  f32,
+	elevator_trim: f32,
+	rudder_trim:   f32,
+}
+
+
+
+// impl<T> Clone for Action<T> {
+//     fn clone(&self) -> Action<T> { *self }
+// }
+
+
+pub trait Environment{
+	fn new() -> Self;
+	
+	fn set_state(&mut self, state: State) -> bool;
+
+    fn reset(&self) -> EnvResult<State>;
+
+	fn step(mut self, action: Action) -> EnvResult<State>;
+
+}
+
+#[derive(Debug)]
+pub struct FlightGear{
+	state: State,
+	action: Action,
+	my_address: SocketAddr,
+	fg_address: SocketAddr,
+    server: Server,
+}
+
+impl FlightGear{
+    fn decode_state(&self, message: &str) -> State {
+        let mut s: Vec<f32> = message.split(';')
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+        State {pitch: s[0], roll: s[1], heading: s[2], altitude: s[3], latitude: s[4], longitude: s[5], a: A{x:s[6],y:s[7],z:s[8]}, v: V{x:s[9],y:s[10],z:s[11]}}
+
+    }
+}
+
+impl Environment for FlightGear {
+	fn new() -> FlightGear {
+		let s = State {pitch: 0.0, roll: 0.0, heading: 0.0, altitude: 0.0, latitude: 0.0, longitude: 0.0, a: A{x:0.0,y:0.0,z:0.0}, v: V{x:0.0,y:0.0,z:0.0}};
+		let a = Action {throttle: 0.0, aileron: 0.0, elevator: 0.0, rudder: 0.0, aileron_trim: 0.0, elevator_trim: 0.0, rudder_trim: 0.0};
+
+        let my_port = 34255;
+        let fg_port = 34256;
+
+        let my_address = SocketAddr::from(([127, 0, 0, 1], my_port));
+        let fg_address = SocketAddr::from(([127, 0, 0, 1], fg_port));
+
+        let server = Server::init(my_address); // "127.0.0.1:34255"
+		FlightGear{state:s, action:a, my_address, fg_address, server}
+	}
+
+	fn set_state(&mut self, state: State) -> bool {
+		self.state = state;
+		true
+	}
+
+	fn reset(&self) -> EnvResult<State> {
+        loop{
+            let incoming = self.server.receive();
+
+            match incoming {
+                Ok(message) => return EnvResult::Some(self.decode_state(&message)),
+                _ => return EnvResult::Done,
+            }
+        }
+        
+		// EnvResult::Some(self.state)
+		// EnvResult::Done
+	}
+
+	fn step(mut self, action: Action) -> EnvResult<State> {
+		EnvResult::Some(self.state)	
+	}
+
+	// fn receive(mut self) -> {
+	// 	self.server.receive();
+	// }
+}
+
+
+
+pub trait Agent{
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,11 @@
-#[derive(Debug, Clone, Copy)]
-struct A{
-	x: f32,
-	y: f32,
-	z: f32,
-}
-
-#[derive(Debug, Clone, Copy)]
-struct V{
-	x: f32,
-	y: f32,
-	z: f32,
-}
-
-#[derive(Debug, Copy, Clone)]
-enum EnvResult<T>{
-	Done,
-	Some(T)
-}
-
-
-#[derive(Debug, Copy, Clone)]
-struct State { // may be Radians for pitch/roll/heading? but for now f32
-	pitch:     f32,
-	roll:      f32,
-	heading:   f32, 
-	altitude:  f32,
-	latitude:  f32,
-	longitude: f32,
-	a:           A,
-	v:           V, 
-}
-
-// impl<T> Clone for State<T> {
-//     fn clone(&self) -> State<T> { *self }
-// }
-
-#[derive(Debug, Copy, Clone)]
-struct Action{
-	throttle:      f32,
-	aileron:       f32,
-	elevator:      f32,
-	rudder:        f32,
-	aileron_trim:  f32,
-	elevator_trim: f32,
-	rudder_trim:   f32,
-}
 
 
 
-// impl<T> Clone for Action<T> {
-//     fn clone(&self) -> Action<T> { *self }
-// }
-
-
-trait Environment{
-	fn new() -> Self;
-	
-	fn set_state(&mut self, state: State) -> bool;
-
-    fn reset(self) -> EnvResult<State>;
-
-	fn step(mut self, action: Action) -> EnvResult<State>;
-
-}
-
-#[derive(Debug, Copy, Clone)]
-struct FlightGear{
-	state: State,
-	action: Action,
-}
-
-impl Environment for FlightGear {
-	fn new() -> FlightGear {
-		let s = State {pitch: 0.0, roll: 0.0, heading: 0.0, altitude: 0.0, latitude: 0.0, longitude: 0.0, a: A{x:0.0,y:0.0,z:0.0}, v: V{x:0.0,y:0.0,z:0.0}};
-		let a = Action {throttle: 0.0, aileron: 0.0, elevator: 0.0, rudder: 0.0, aileron_trim: 0.0, elevator_trim: 0.0, rudder_trim: 0.0};
-		FlightGear{state:s, action:a}
-	}
-
-	fn set_state(&mut self, state: State) -> bool {
-		self.state = state;
-		true
-	}
-
-	fn reset(self) -> EnvResult<State> {
-		EnvResult::Some(self.state)
-		// EnvResult::Done
-	}
-
-	fn step(mut self, action: Action) -> EnvResult<State> {
-		EnvResult::Some(self.state)	
-	}
-}
-
-
-
-pub trait Agent{
-
-}
 
 mod lib;
 use lib::serve;
+use lib::{Environment, FlightGear, Server, EnvResult, State, Action};
+// use lib::EnvResult;
 
 fn main() {
 	println!("Kek!");
@@ -120,7 +26,14 @@ fn main() {
 		EnvResult::Some(state) => println!("state: {:?}", state),
 		EnvResult::Done => println!("DONE!"),
 	}
-	let served = serve();
-	println!("fg: {:?}", served)
+	// let served = serve();
+	// println!("fg: {:?}", served)
+	// let s1 = Server::init("127.0.0.1:34255");
+	// let s2 = Server::init("127.0.0.1:34255");
+	// let kek = s1.receive().unwrap();
+	// println!("{}", kek);
+
+	// let result = s1.send(&kek, "127.0.0.1:");
+	// println!("Result: {:?}", result);
 
 }
