d4605c540581485611538be7b29a6a2e72f07e8a
kekonen <daniil.naumetc@gmail.com>
2019-01-28 19:53:34 +0100

    comment out serve function

--- src/lib.rs
+++ src/lib.rs
@@ -8,23 +8,23 @@ use std::num::ParseFloatError;
 // #![feature(rustc_private)]
 // #[macro_use] extern crate log;
 
-pub fn serve() -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind("127.0.0.1:34254")?;
-        //  ncat -v localhost 34254 -u
+// pub fn serve() -> std::io::Result<()> {
+//     {
+//         let socket = UdpSocket::bind("127.0.0.1:34254")?;
+//         //  ncat -v localhost 34254 -u
 
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-        let mut buf = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buf)?;
+//         // Receives a single datagram message on the socket. If `buf` is too small to hold
+//         // the message, it will be cut off.
+//         let mut buf = [0; 10];
+//         let (amt, src) = socket.recv_from(&mut buf)?;
 
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        socket.send_to(buf, &src)?;
-    } // the socket is closed here
-    Ok(())
-}
+//         // Redeclare `buf` as slice of the received data and send reverse data back to origin.
+//         let buf = &mut buf[..amt];
+//         buf.reverse();
+//         socket.send_to(buf, &src)?;
+//     } // the socket is closed here
+//     Ok(())
+// }
 
 #[derive(Debug)]
 pub struct Server{
