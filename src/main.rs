d65bf64220df562a9080e0ec6e64337358ea52aa
kekonen <daniil.naumetc@gmail.com>
2019-01-29 21:01:17 +0100

    roll pid ok

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
-// extern crate pid_control;
+extern crate pid_control;
 
-// use pid_control::PIDController;
-// use pid_control::Controller;
+use pid_control::PIDController;
+use pid_control::Controller;
 
 
 
@@ -16,22 +16,31 @@ fn main() {
 
 	let mut a = Agent::init(&mut e);
 
-	// let mut p = PIDController::new(0.1, 0.2, 0.5);
+	let mut roll_pid = PIDController::new(0.003, 0.002, 0.003);
+	// let mut roll_pid = PIDController::new(0.003, 0.003, 0.004);
+	// let mut roll_pid = PIDController::new(0.01, 0.003, 0.004);
 
-    // p.set_target(0.);
+    roll_pid.set_target(15.0);
 
 
 	a.run(|state, paction| {
 		let paction = *paction;
-		// match paction {
-		// 	PossibleAction::Some(mut action) => {
-				
-		// 		action.aileron +=0.01;
-		// 		return PossibleAction::Some(action)
-		// 	},
-		// 	PossibleAction::None => return PossibleAction::None,
-		// }
-		PossibleAction::None
+		let paction = match paction {
+			PossibleAction::Some(mut action) => {
+				// println!("action : {:?}", action);
+				let control_input = roll_pid.update(state.roll as f64, 0.1) as f32;
+				println!("c: {}", control_input);
+				action.aileron += control_input;
+
+				PossibleAction::Some(action)
+			},
+			PossibleAction::None => PossibleAction::None,
+		};
+
+		println!("p: {}, r: {}, h: {}, action: {:?}", format!("{:.*}", 2, state.pitch), format!("{:.*}", 2, state.roll), format!("{:.*}", 2, state.heading), paction);
+
+		paction
+		// PossibleAction::None
 
 	});
 
