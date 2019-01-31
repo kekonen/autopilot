8de7d3ee498ed3d5b124cd0cb4cab0f170bd8646
kekonen <daniil.naumetc@gmail.com>
2019-02-01 00:31:15 +0100

    can nicely follow waypoint

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 	roll_pid.set_limits(-45.0,45.0);
     roll_pid.set_target(desired_roll_angle);
 
-	let mut pitch_pid = PIDController::new(-0.01, -0.007, 0.007);
+	let mut pitch_pid = PIDController::new(-0.01, -0.001, 0.01);
 	// let mut pitch_pid = PIDController::new(-0.003, -0.001, -0.008);
 	// let mut pitch_pid = PIDController::new(-0.003, -0.0017, -0.01);
 	// let mut pitch_pid = PIDController::new(-0.0003, -0.0001, -0.01);
@@ -35,7 +35,8 @@ fn main() {
 	roll_pid.set_limits(-1.0,1.0);
     pitch_pid.set_target(0.0);
 
-	let mut bank_turn_pid = PIDController::new(-0.1, -0.2, 0.7);
+	let mut bank_turn_pid = PIDController::new(-0.3, -0.00, -0.0);
+	// let mut bank_turn_pid = PIDController::new(-0.1, -0.06, -0.005); //better
 	// let mut bank_turn_pid = PIDController::new(-0.08, -0.07, -0.09);  //strange but close
 	// let mut bank_turn_pid = PIDController::new(-0.003, 0.0001, 0.0001); // but after reaching end goes bad
 	roll_pid.set_limits(-1.0,1.0);
@@ -48,20 +49,20 @@ fn main() {
 			PossibleAction::Some(mut action) => {
 				let dest_delta_heading = n.get_delta_heading_to_destination(state.gps_latitude, state.gps_longitude, state.heading);
 
-				// let turn_input = bank_turn_pid.update(dest_delta_heading as f64, 0.1) as f32;
-				// desired_roll_angle += turn_input as f64;
-				// roll_pid.set_target(turn_input as f64);
+				
+				let turn_input = bank_turn_pid.update(dest_delta_heading as f64, 0.1) as f32;
+				roll_pid.set_target(turn_input as f64);
 				
 				// println!("action : {:?}", action);
 				let roll_input = roll_pid.update(state.roll as f64, 0.1) as f32;
-				println!("ri: {}, ra: {}", roll_input, state.roll);
+				// println!("ri: {}, ra: {}", roll_input, state.roll);
 				action.aileron = roll_input;
 
 				let pitch_input = pitch_pid.update(state.pitch as f64, 0.1) as f32;
-				println!("pi: {}, pa: {}", pitch_input, state.pitch);
+				// println!("pi: {}, pa: {}", pitch_input, state.pitch);
 				action.elevator = pitch_input;
 
-				// println!("ti: {}, dra: {}, dh: {}, ri: {}, pi: {}", format!("{:.*}", 4, turn_input), format!("{:.*}", 2, desired_roll_angle), format!("{:.*}", 2, dest_delta_heading), format!("{:.*}", 4, roll_input), format!("{:.*}", 4, pitch_input));
+				println!("ti: {}, dra: {}, dh: {}, ri: {}, pi: {}", format!("{:.*}", 4, turn_input), format!("{:.*}", 2, desired_roll_angle), format!("{:.*}", 2, dest_delta_heading), format!("{:.*}", 4, roll_input), format!("{:.*}", 4, pitch_input));
 
 				PossibleAction::Some(action)
 				// PossibleAction::None
