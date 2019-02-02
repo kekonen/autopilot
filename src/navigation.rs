2a34766697450b96b3abe282ad35368a19e61c9a
kekonen <daniil.naumetc@gmail.com>
2019-02-02 23:04:26 +0100

    fixed some destination distance sintax probs

--- src/navigation.rs
+++ src/navigation.rs
@@ -11,7 +11,7 @@ impl Location{
         Location {latitude, longitude}
     }
 
-    pub fn distance_to(&mut self, other: Location) -> f32 {
+    pub fn distance_to(&self, other: &Location) -> f32 {
         let d2r = f32::consts::PI / 180.0;
 
         let dlong = (self.longitude - other.longitude) * d2r;
@@ -36,7 +36,7 @@ impl Navigation{
         Navigation { dest_location: Location::new(0.0, 0.0)}
     }
 
-    pub fn get_delta_heading_to_destination(&mut self, current_location: Location, heading: f32) -> f32 {
+    pub fn get_delta_heading_to_destination(&mut self, current_location: &Location, heading: f32) -> f32 {
         // delta_lat = self.dest_latitude - gps_latitude
         let delta_latitude = self.dest_location.latitude - current_location.latitude;
 
@@ -63,6 +63,10 @@ impl Navigation{
         if delta_destination_heading < -180.0 { delta_destination_heading += 360.0 }
 
         delta_destination_heading
+    }
+
+    pub fn get_dest_location(&mut self) -> &Location{
+        &self.dest_location
     }
 
     pub fn set_dest_location(&mut self, new_location: Location) {
